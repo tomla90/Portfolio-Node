8174e7e53c843873eb7e8c225c52d8520fb193b7
Tom Granberg <tom.andre.granberg@gmail.com>
2023-01-16 12:04:28 +0100

    Add Bootstrap-icons

--- app.js
+++ app.js
@@ -21,6 +21,7 @@ app.use(express.static(path.join(__dirname, 'public')));
 app.use(express.static(__dirname + '/node_modules/bootstrap/dist'));
 app.use(express.static(__dirname + '/node_modules/jquery/dist/'));
 app.use(express.static(__dirname + '/node_modules/typed.js/lib'));
+app.use(express.static(__dirname + '/node_modules/bootstrap-icons'));
 
 app.use('/', indexRouter);
 app.use('/users', usersRouter);
