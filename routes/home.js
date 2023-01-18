701f4a54d745cb84f6fc7237bda7e1720de02dec
Tom Granberg <tom.andre.granberg@gmail.com>
2023-01-18 10:22:34 +0100

    Add handling new introductions

--- routes/home.js
+++ routes/home.js
@@ -2,11 +2,24 @@ var express = require('express');
 var router = express.Router();
 const fs = require("fs")
 const path = require("path")
+var bodyParser = require('body-parser')
+var jsonParser = bodyParser.json()
+
+
+router.post('/', jsonParser, function(req, res, next) {
+  let rawdata = fs.readFileSync(path.resolve(__dirname, "../data/introductionArray.json"));
+  let array = JSON.parse(rawdata);
+  const newArray = array.concat([req.body.newText])
+  fs.writeFileSync(path.resolve(__dirname, "../data/introductionArray.json"), JSON.stringify(newArray));
+  res.end();
+});
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
   let data = fs.readFileSync(path.resolve(__dirname, "../data/introductionArray.json"));
   res.render('home', {array:JSON.parse(data)});
 });
+
+
 
 module.exports = router;
