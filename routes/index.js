1d5fab177bcaefb59e09c5094c37cb78eefddc6b
Tom Granberg <tom.andre.granberg@gmail.com>
2023-01-18 12:27:22 +0100

    Refactor recommendations

--- routes/index.js
+++ routes/index.js
@@ -5,9 +5,9 @@ const path = require("path")
 
  /* GET home page. */
  router.get('/', function(req, res, next) {
-  let data = fs.readFileSync(path.resolve(dirname, "../data/introductionArray.json"));
-  let datarecommend = fs.readFileSync(path.resolve(dirname, "../data/recommendations.json"));
-  res.render('index', { title: 'Express' , array: JSON.parse(data), datarecommend: JSON.parse(datarecommend)});
+  let data = fs.readFileSync(path.resolve(__dirname, "../data/introductionArray.json"));
+  let datarecommend = fs.readFileSync(path.resolve(__dirname, "../data/recommendations.json"));
+  res.render('index', { title: 'Express' , array: JSON.parse(data), data: JSON.parse(datarecommend)});
 });
 
 
