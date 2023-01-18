45072817172f867680a6fccbd77ff7598fb251b8
Tom Granberg <tom.andre.granberg@gmail.com>
2023-01-18 12:41:30 +0100

    Add POST and Delete recommendations handlers

--- routes/recommendations.js
+++ routes/recommendations.js
@@ -2,12 +2,34 @@ var express = require('express');
 var router = express.Router();
 const fs = require("fs")
 const path = require("path")
+var bodyParser = require('body-parser')
+var jsonParser = bodyParser.json()
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
   let data = fs.readFileSync(path.resolve(__dirname, "../data/recommendations.json"));
   res.render('recommendations', { data: JSON.parse(data)
  });
+});
+
+router.post('/', jsonParser, function(req, res, next) {
+  let rawdata = fs.readFileSync(path.resolve(__dirname, "../data/recommendations.json"));
+  let recommendationsArray = JSON.parse(rawdata);
+  if(recommendationsArray.filter(x => x.name === req.body.name).length == 0) {
+    const newArray = recommendationsArray.concat([req.body])
+    fs.writeFileSync(path.resolve(__dirname, "../data/recommendations.json"), JSON.stringify(newArray));
+  }
+  res.end();
+});
+
+router.delete('/', jsonParser, function(req, res, next) {
+  let rawdata = fs.readFileSync(path.resolve(__dirname, "../data/recommendations.json"));
+  let recommendationsArray = JSON.parse(rawdata);
+  const newArray = recommendationsArray.filter(x => x.name !== req.body.name)
+  if (newArray.length !== recommendationsArray.length ) {
+    fs.writeFileSync(path.resolve(__dirname, "../data/recommendations.json"), JSON.stringify(newArray));
+  }
+  res.end();
 });
 
 module.exports = router;
