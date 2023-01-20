0b8e506bc7698c72d685891477c0219490c751b6
Tom Granberg <tom.andre.granberg@gmail.com>
2023-01-20 10:17:27 +0100

    Portfolio and recommendations validation

--- routes/recommendations.js
+++ routes/recommendations.js
@@ -13,23 +13,58 @@ router.get('/', function(req, res, next) {
 });
 
 router.post('/', jsonParser, function(req, res, next) {
-  let rawdata = fs.readFileSync(path.resolve(__dirname, "../data/recommendations.json"));
-  let recommendationsArray = JSON.parse(rawdata);
-  if(recommendationsArray.filter(x => x.name === req.body.name).length == 0) {
-    const newArray = recommendationsArray.concat([req.body])
-    fs.writeFileSync(path.resolve(__dirname, "../data/recommendations.json"), JSON.stringify(newArray));
+  //Check if the attributes of req body are valid
+  const expectedAttributes = ["name", "avatar", "role", "description"];
+  if (!Object.keys(req.body).every(param => expectedAttributes.includes(param))) {
+    return res.status(400).send({ error: "Unexpected attribute provided" });
   }
-  res.end();
+  // Check if the name and avatar attributes are provided
+  if(!req.body.name || !req.body.avatar) {
+    return res.status(400).send({ error: "Name and avatar not provided" });
+  }
+  // Check if the avatar is in the array [1,2,3]
+  if(!([1,2,3].includes(req.body.avatar))) {
+    return res.status(400).send({ error: "Invalid avatar value" });
+  }
+  // read the recommendations.json file and parse it 
+  fs.promises.readFile(path.resolve(__dirname, "../data/recommendations.json"), 'utf8')
+    .then(rawdata => {
+      let recommendationsArray = JSON.parse(rawdata);
+      // check if the name is already in the recommendations array
+      if(recommendationsArray.filter(x => x.name === req.body.name).length == 0) {
+        // if not, add it to the array
+        const newArray = recommendationsArray.concat([req.body])
+        //write the new array to the recommendations.json file
+        return fs.promises.writeFile(path.resolve(__dirname, "../data/recommendations.json"), JSON.stringify(newArray));
+      }
+    }).then(() => {
+      //end the response 
+      res.end();
+    }).catch(err => {
+      return res.status(500).send({ error: "Failed to read or write recommendations data"});
+    });
 });
 
 router.delete('/', jsonParser, function(req, res, next) {
-  let rawdata = fs.readFileSync(path.resolve(__dirname, "../data/recommendations.json"));
-  let recommendationsArray = JSON.parse(rawdata);
-  const newArray = recommendationsArray.filter(x => x.name !== req.body.name)
-  if (newArray.length !== recommendationsArray.length ) {
-    fs.writeFileSync(path.resolve(__dirname, "../data/recommendations.json"), JSON.stringify(newArray));
+  //Check if the name attribute is provided
+  if(!req.body.name) {
+    return res.status(400).send({ error: "Name not provided" });
   }
-  res.end();
+  //read the recommendations.json file and parse it 
+  fs.promises.readFile(path.resolve(__dirname, "../data/recommendations.json"), 'utf8')
+    .then(rawdata => {
+      let recommendationsArray = JSON.parse(rawdata);
+      // filter out the recommendation with the name provided in the req body
+      const newArray = recommendationsArray.filter(x => x.name !== req.body.name)
+      //write the new array to the recommendations.json file
+      if (newArray.length !== recommendationsArray.length ) {
+        return fs.promises.writeFile(path.resolve(__dirname, "../data/recommendations.json"), JSON.stringify(newArray));
+      }
+    }).then(() => {
+      res.end();
+    }).catch(err => {
+      return res.status(500).send({ error: "Failed to read or write recommendationsdata data"});
+    });
 });
 
 module.exports = router;
